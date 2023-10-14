02614ce90580f37c91991c6b35d4cc745b8a76aa
Zaaks-Project <zaakirah.abrams@younglings.africa>
2023-10-14 15:13:09 +0200

    Latest Updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react"
+import './Stylesheet.css'
+import About from "./About"
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <>
+        <div className="hero">
+            <div className="hero_container">
+                <h1>Research, Design, Visual Styling</h1>
+                <h2>My Mind Through Design & More</h2>
+                <button className="more_btn">Check It Out</button>
+            </div>
+        </div>
+        
+        </>
     )
 }
 
