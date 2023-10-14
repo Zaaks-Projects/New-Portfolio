6f2595c89f15e4cae0ef7c51fbd0dad0fc8e119c
Zaaks-Project <zaakirah.abrams@younglings.africa>
2023-10-14 14:03:29 +0200

    Adding some new pages to the website as well as routing the pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './pages/Home'
+import About from './pages/About';
+import Work from './pages/Work';
+import Resume from './pages/Resume';
 
 
 function App() {
+  let component
+  switch (window.location.pathname) {
+    case "/" :
+      component = <Home />
+      break
+    case "/about" :
+      component = <About />
+      break
+    case "/work" :
+      component = <Work />
+      break
+    case "/resume" :
+      component = <Resume />
+      break
+  }
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      {component}
+      
+    </>
+    
   );
 }
 
