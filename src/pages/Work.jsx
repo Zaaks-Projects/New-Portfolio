be7a3bd3297282f5d77902dbd5e3e56deece5ec6
Zaaks-Project <zaakirah.abrams@younglings.africa>
2023-10-15 17:30:39 +0200

    Latest Update | Work page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -11,24 +11,28 @@ const Work = () => {
             <div className="grid-item">
                 <div>
                 <h3>Storyboarding</h3>
+                <i class="bi bi-sticky-fill"></i>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt necessitatibus, commodi temporibus culpa ipsum explicabo, magni expedita illum ipsam accusamus debitis similique dolore architecto laboriosam tempore quod veniam, totam id.</p>
                 </div>
             </div>
             <div className="grid-item">
                 <div>
                 <h3>Creating Personas</h3>
+                <i class="bi bi-person-bounding-box"></i>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quia doloremque similique recusandae nesciunt culpa quam excepturi velit beatae, voluptates voluptatum magni assumenda aliquam vero totam vitae distinctio rerum dolorum.</p>
                 </div>
             </div>
             <div className="grid-item">
                 <div>
                 <h3>Wireframing</h3>
+                <i class="bi bi-vector-pen"></i>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus perferendis id nesciunt similique veniam voluptatum odio temporibus fugit aspernatur aliquam sequi labore nemo facere velit, tempora libero totam quibusdam saepe.</p>
                 </div>
             </div>
             <div className="grid-item">
                 <div>
                 <h3>Prototyping</h3>
+                <i class="bi bi-bezier2"></i>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque corporis officiis perspiciatis eos voluptas ea libero, commodi illum possimus. Maxime exercitationem assumenda expedita hic. Temporibus et harum nostrum enim dolorem!</p>
                 </div>
             </div>
