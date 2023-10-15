ac76bec388606400136dac5668622ac6409af4d0
Zaaks-Project <zaakirah.abrams@younglings.africa>
2023-10-15 12:26:45 +0200

    Latest Update |

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,23 @@
 import React from "react"
+import './Stylesheet.css'
 
 const About = () => {
     return (
-        <h1>About</h1>
+        <div className="project_overview">
+            <div>
+                <h1>
+                    Zaakirah Abrams
+                </h1>
+            </div>
+            <div>
+               <p>
+               Hello there, thanks for taking the time to view my portfolio
+               </p>
+               <p>
+               I'm currently situated in Cape Town, South Africa, pursuing my desired liking into everything basically<span>design</span>. My Hobbies include working with a camera, catching up with my favourite tv series, 
+               </p>
+            </div>
+        </div>
     )
 }
 
