0ea9594b35e542c30bc6463f02acccb42fb5cf5d
Zaaks-Projects <zaakirah.abrams@younglings.africa>
2023-10-16 16:17:46 +0200

    Latest Update | Added a style to the cursor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,62 @@
-import React from "react"
-import './Stylesheet.css'
-import Work_Section from "./work-section"
-import Contact_Section from "./contact_section"
-import Footer from "./Footer"
+import React, { useEffect, useState } from "react";
+import "./Stylesheet.css";
+import Work_Section from "./work-section";
+import Contact_Section from "./contact_section";
+import Footer from "./Footer";
 // import AOS from 'aos';
 // import 'aos/dist/aos.css';
 
 const Home = () => {
-    
-    return (
-        <>
-        <div className="hero" data-aos="fade-down">
-            <div className="hero_container">
-                <h1>Research, Design, Visual Styling</h1>
-                <h2>My Mind Through Design & More</h2>
-                <button className="more_btn">Check It Out</button>
-            </div>
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const calculateGradient = () => {
+    // Adjust these values for the central color and fade color
+    const centralColor = "#14b9ff"; // Blue color in this example
+    const fadeColor = "rgba(20, 185, 255, 0)"; // Fully transparent color at the edge
+
+    const gradient = `radial-gradient(${centralColor}, ${fadeColor} 100%)`;
+
+    return gradient;
+  };
+
+  const circleStyle = {
+    position: "fixed",
+    top: mousePosition.y - 50,
+    left: mousePosition.x - 50,
+    width: "100px",
+    height: "100px",
+    borderRadius: "50%",
+    background: calculateGradient(),
+    pointerEvents: "none",
+    zIndex: -1,
+  };
+  return (
+    <>
+      <div style={circleStyle}></div>
+      <div className="hero" data-aos="fade-down">
+        <div className="hero_container">
+          <h1>Research, Design, Visual Styling</h1>
+          <h2>My Mind Through Design & More</h2>
+          <button className="more_btn">Check It Out</button>
         </div>
-        < Work_Section />
-        < Contact_Section />
-        <Footer />
-        </>
-    )
-}
+      </div>
+      <Work_Section />
+      <Contact_Section />
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
