37f877c02b3da343f69c00de40f33fb4be3a2201
Zaaks-Project <zaakirah.abrams@younglings.africa>
2023-10-17 16:33:54 +0200

    Latest Update | Work page almost finished added the resume to the website

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import CV from "./images/Zaakirah Abrams.png";
 import "./Stylesheet.css";
+import Footer from "./Footer";
+import CV from "./images/Zaakirah Abrams.png";
 
 const Resume = () => {
-  return <img className="CV" src={CV} alt='' />;
+  return (
+    <>
+          <img className='CV' src={CV} alt='' />
+          < Footer />
+    </>
+  );
 };
 
 export default Resume;
