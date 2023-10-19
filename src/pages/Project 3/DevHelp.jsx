08ed3b3b5e2c0152555de836cf8c16f1e73f15ae
Zaaks-Project <zaakirah.abrams@younglings.africa>
2023-10-19 16:09:12 +0200

    Latest Update | Second Project

--- src/pages/Project 3/DevHelp.jsx
+++ src/pages/Project 3/DevHelp.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./DevHelp.css";
-import Background from "./devHelp Background.png";
-
+import Persona from './Persona.png'
+  
+  
+  
+  
 const DevHelp = () => {
   return (
     <>
@@ -27,59 +30,81 @@ const DevHelp = () => {
         <div>
           <h1>Project Overview</h1>
           <p>
-            There is a large population worldwide that remains food insecure. We
-            were tasked to create a food delivery app that could generate food
-            sources for food-insecure individuals. Our app provides food sources
-            for those who are food insecure or currently have a low income. We
-            achieve this by supporting local restaurants and partnering with
-            them to receive donations.
+            devHelp! is an app created with the intention of helping those
+            trying to gain some work experience. Many entry-level developers
+            face challenges in landing their dream jobs due to their limited
+            experience. This is why we have developed an app aimed at providing
+            them with opportunities to gain valuable experience and skills.
           </p>
         </div>
         <div>
           <h1>Problem</h1>
           <p>
-            There is a large population worldwide that remains food insecure.
-            “Food insecure” is a term that refers to those who are unsure of
-            where their next meal can be or those who do not have ready access
-            to healthy & nutritious food. Here is a link that goes more into
-            detail regarding this issue and populations who are affected:
-            <span>https://sentientmedia.org/food-insecurity/</span>
+            With research done, I noticed that the unemployment rate in South
+            Africa is very high. Often, we have developers who don't have the
+            experience needed for a particular position. They may be at a junior
+            or mid-senior level of skill. That's why our team collectively
+            decided to develop an app that offers developers the opportunity to
+            gain valuable experience through part-time or freelance projects.
           </p>
         </div>
       </div>
-      <section className='research_section'>
-        <h1>01. Research Conducted</h1>
+      <section className='research_section_devHelp'>
+        <h1>Goals for this project</h1>
         <div className='line_devHelp'></div>
-        <div className='discover'>
-          <div>
-            <h1>Mission</h1>
-          </div>
-          <div>
-            <p>Help those who are food insecure</p>
-            <p>
-              <span>a.</span> Build community
-            </p>
-            <p>
-              <span>b.</span>Reduce Food waste
-            </p>
-          </div>
-          <div>
-            <h1>Target Audience</h1>
-          </div>
-          <div>
-            <p>Humanitarians</p>
-            <p>Activist</p>
-            <p>Community Centered</p>
-          </div>
-          <div>
-            <h1>Restaurants the app would have </h1>
-          </div>
-          <div>
-            <p>Local Restaurants</p>
-            <p>Small Businesses</p>
-          </div>
+        <div>
+          <p>
+            1. Target audience: We intend to target those living in South
+            Africa.
+          </p>
+          <p>
+            2. Users Goals: To have them explore company developer opportunities
+            or gain the chance to experience freelance projects hosted by
+            founders.
+          </p>
+          <p>3. Business Goals: To introduce developers to various companies</p>
         </div>
       </section>
+      <div className='discover_phase_devHelp'>
+        <h1>Discovery Phase</h1>
+        <p>Throughout our reaserach, here's what motivated us</p>
+      </div>
+      <div className='metrics_devHelp'>
+        <div>
+          <h1>Mission Statement</h1>
+          <p>
+            Facilitate individuals seeking to acquire valuable experience for
+            better job prospects:
+          </p>
+          <p>a. Build community</p>
+          <p>b. Gain Experience</p>
+          <p>c. Get Recognition</p>
+        </div>
+        <div>
+          <h1>Metrics</h1>
+          <p>
+            According to recent metrics, there are over 133195 unfilled software
+            positions in South Africa alone. South Africa consistently ranks 1st
+            on the list of software development countries in Africa. The problem
+            is that most of these software positions require an average of 5
+            years minimum to apply. A large obstacle for industry newcomers.
+          </p>
+        </div>
+        <div>
+          <h1>Solution</h1>
+          <p>
+            This is where devHelp comes in. We offer a special place for
+            developers where they can use their expertise on small projects offered by other devs or clients that they can add to their
+            development portfolio.
+            Very much in the vein of Offer Zen or
+            LinkedIn but tailored specifically for developers.
+          </p>
+        </div>
+      </div>
+      <img className="persona" src={Persona} alt="" />
+      <div className="discover_phase_devHelp">
+        <h1>Design Phase of the Project</h1>
+      </div>
     </>
   );
 };
